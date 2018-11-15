f8b121c7e07c4a3733e920840de64157996b6040
yashj645 <32262757+yashj645@users.noreply.github.com>
2018-11-15 22:09:27 +0530

    Update Experiment 13.2.cpp

--- Experiment 13.2.cpp
+++ Experiment 13.2.cpp
@@ -6,12 +6,12 @@ int a[10][10],i,j,k,n,stk[10],top,v,visit[10],visited[10],x,y;
 
 main()
 {
-cout <<"enter no. of vertices SIR/MAM!";
+cout <<"enter no. of vertices";
 cin >> n;
-cout<<"sir please enter for undirected grah"<<endl;
+cout<<"enter for undirected grah"<<endl;
      cin>>x>>y;
     if(x>=n&&y>=n)
-        cout<<"sir ji please enter within range"<<endl;
+        cout<<"please enter within range"<<endl;
         else
         {
     while(x!=-1&&y!=-1)
@@ -24,7 +24,7 @@ cout<<"sir please enter for undirected grah"<<endl;
     }
 cout <<"enter initial vertex";
 cin >>v;
-cout <<"AFTER DFS ,SIR ORDER COMES -:"<<endl;
+cout <<"AFTER DFS"<<endl;
 cout << v <<" ";
 visited[v]=1;
 k=1;
